a2f4d44e89fd4179eeb4cb875008a58515bff6fb
D Arun David Reddy <47513825+daviddodda1@users.noreply.github.com>
2025-02-03 11:34:17 +0000

    temp

--- keyboards/sofle/rev1/keymaps/vial/keymap.c
+++ keyboards/sofle/rev1/keymaps/vial/keymap.c
@@ -19,7 +19,7 @@
 // clang-format off
 
 #include QMK_KEYBOARD_H
-#include "oled.c"
+// #include "oled.c"
 
 // Default keymap. This can be changed in Vial. Use oled.c to change beavior that Vial cannot change.
 
