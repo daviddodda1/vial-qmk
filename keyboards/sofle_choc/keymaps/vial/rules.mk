011714e269c07e1e5bd5b9594c2f38a98c6cf7e9
D Arun David Reddy <47513825+daviddodda1@users.noreply.github.com>
2025-02-03 12:16:04 +0000

    new

--- keyboards/sofle_choc/keymaps/vial/rules.mk
+++ keyboards/sofle_choc/keymaps/vial/rules.mk
@@ -1,7 +1,7 @@
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes
 CONSOLE_ENABLE = no
-EXTRAKEY_ENABLE = yes
+EXTRAKEY_ENABLE = no
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 ENCODER_MAP_ENABLE = no
@@ -14,8 +14,7 @@ MOUSEKEY_ENABLE = no
 COMBO_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
 RGB_MATRIX_ENABLE = no
-SPLIT_KEYBOARD = yes	# Split keyboard
-WPM_ENABLE = yes		# WPM counter for keyboard pets (+1298)
+WPM_ENABLE = no		# WPM counter for keyboard pets (+1298)
 
 
 MCU = atmega32u4
